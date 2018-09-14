22604db7fb5c9a51de4fb9dc9d4b95767adf2548
jollyjohnny <JohnEastCodeStock@gmail.com>
2018-09-15 00:04:16 +0800

    re commit

--- BuildData/BuildNameList.py
+++
@@ -1,8 +0,0 @@
-## 生成 Data/stockList 所有股票名称
-
-from lib import loginTusharePro
-pro = loginTusharePro.login()
-
-
-data = pro.stock_basic(exchange_id='', is_hs='', fields='ts_code,symbol,name,exchange_id,list_date,list_status')
-data.to_csv('../myData/stockList.csv')
